fa032d37f47fdd6db00b887f5cd864c25452a696
felix <felix@hpcnt.com>
2018-10-05 17:48:10 +0900

    Archive ReactorKit and ReactorKitRuntime

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,22 +6,27 @@ cd Build/ReactorKit
 # Dynamic framework
 rm -rf Carthage/
 carthage build --no-skip-current --verbose | xcpretty -c
-carthage archive ReactorKit
+carthage archive ReactorKit ReactorKitRuntime
 cp ReactorKit.framework.zip ../../ReactorKit.framework.zip
 
 # Static framework
 rm -rf Carthage/
 
+cp ../../scripts/ld.py ./ld.py
 xcconfig=$(mktemp /tmp/static.xcconfig.XXXXXX)
 trap 'rm -f "$xcconfig"' INT TERM HUP EXIT
 
-cp ../../ld.py ./ld.py
 echo "LD = ${PWD}/ld.py" >> $xcconfig
+echo "MACH_O_TYPE = staticlib" >> $xcconfig
 echo "DEBUG_INFORMATION_FORMAT = dwarf" >> $xcconfig
 
 export XCODE_XCCONFIG_FILE="$xcconfig"
-carthage build ReactorKit --no-skip-current --verbose | xcpretty -c
-carthage archive ReactorKit
+carthage build --no-skip-current --verbose | xcpretty -c
+mv Carthage/Build/Mac/Static/* Carthage/Build/Mac/
+mv Carthage/Build/iOS/Static/* Carthage/Build/iOS/
+mv Carthage/Build/tvOS/Static/* Carthage/Build/tvOS/
+mv Carthage/Build/watchOS/Static/* Carthage/Build/watchOS/
+carthage archive ReactorKit ReactorKitRuntime
 cp ReactorKit.framework.zip ../../ReactorKit-Static.framework.zip
 
 cd ../..
